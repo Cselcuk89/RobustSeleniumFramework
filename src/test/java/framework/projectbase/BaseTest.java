fb4eac4931ef1301bd1848b0d767e3aee35ed2ab
Cselcuk89 <cagriselcuk89@gmail.com>
2025-09-13 14:31:05 -0500

    Commit : Add base classes

---
+++ src/test/java/framework/projectbase/BaseTest.java
@@ -0,0 +1,101 @@
+package framework.projectbase;
+
+import framework.frameworkfactory.abstractFactory.DriverManagerAbstract;
+import framework.frameworkfactory.abstractFactory.DriverManagerFactoryAbstract;
+import framework.frameworkutils.CookieHelper;
+import framework.projectconstants.DriverType;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import io.restassured.http.Cookies;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.Cookie;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.testng.ITestResult;
+import org.testng.annotations.*;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+import ru.yandex.qatools.ashot.AShot;
+import ru.yandex.qatools.ashot.Screenshot;
+import ru.yandex.qatools.ashot.shooting.ShootingStrategies;
+
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+public class BaseTest {
+    protected ThreadLocal<DriverManagerAbstract> driverManager = new ThreadLocal<>();
+    protected ThreadLocal<WebDriver> driver = new ThreadLocal<>();
+
+    @Parameters("browser")
+    @BeforeMethod
+    public synchronized void startDriver(@Optional String browser) {
+        browser = System.getProperty("browser", browser);
+//        if (browser == null) browser = "CHROME";
+//        setDriver(new DriverManagerOriginal().initializeDriver(browser));
+//        setDriver(DriverManagerFactory.getManager(DriverType.valueOf(browser)).createDriver());
+        setDriverManager(DriverManagerFactoryAbstract.
+                getManager(DriverType.valueOf(browser)));
+        setDriver(getDriverManager().getDriver());
+        System.out.println("Current Thread: " + Thread.currentThread().getId() + ", " + "DRIVER = " + getDriver());
+    }
+
+    @Parameters("browser")
+    @AfterMethod
+    public synchronized void quitDriver(@Optional String browser, ITestResult result) throws IOException {
+        if (result.getStatus() == ITestResult.FAILURE) {
+            File destFile = new File("screenshots" + File.separator + browser + File.separator +
+                    result.getTestClass().getRealClass().getSimpleName() + "_" + result.getMethod().getMethodName() + ".png");
+//            takeScreenshot(destFile);
+            takeScreenshotUsingAshot(destFile);
+        }
+        getDriverManager().getDriver().quit();
+    }
+
+    @AfterClass
+    public void tearDownClass() {
+        WebDriverManager.chromedriver().clearDriverCache();
+    }
+
+    public void injectCookiesToBrowser(Cookies cookies) {
+        List<org.openqa.selenium.Cookie> seleniumCookies = new CookieHelper().convertRestAssuredCookiesToSeleniumCookies(cookies);
+        for (Cookie cookie : seleniumCookies) {
+            getDriver().manage().addCookie(cookie);
+        }
+    }
+
+    protected DriverManagerAbstract getDriverManager() {
+        return this.driverManager.get();
+    }
+
+    private void setDriverManager(DriverManagerAbstract driverManager) {
+        this.driverManager.set(driverManager);
+    }
+
+    protected WebDriver getDriver() {
+        return this.driver.get();
+    }
+
+    private void setDriver(WebDriver driver) {
+        this.driver.set(driver);
+    }
+
+    private void takeScreenshot(File destFile) throws IOException {
+        TakesScreenshot takesScreenshot = (TakesScreenshot) getDriver();
+        File srcFile = takesScreenshot.getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(srcFile, destFile);
+    }
+
+    private void takeScreenshotUsingAshot(File destFile) {
+        Screenshot screenshot = new AShot()
+                .shootingStrategy(ShootingStrategies.viewportPasting(100))
+                .takeScreenshot(getDriver());
+        try {
+            ImageIO.write(screenshot.getImage(), "PNG", destFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
